2a507af69a16e79c4409f22d5440b09e7fffc2a7
RozelleRZ <rohit.bandaru5@gmail.com>
2022-05-14 00:00:41 +0530

    final commit

--- fracture_harris.py
+++ fracture_harris.py
@@ -4,14 +4,7 @@ import matplotlib.pyplot as plt
 import easygui
    
 
-def Canny_detector(img, weak_th = None, strong_th = None):
-      
-    # conversion of image to grayscale
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-      
-    # noise reduction
-    #img = cv2.bilateralFilter(img,9,75,75)
+def CannyDetector(img, weak_th = None, strong_th = None):
        
     # gradient calculation
     gx = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
@@ -93,15 +86,15 @@ def Canny_detector(img, weak_th = None, strong_th = None):
     # returning the magnitude of gradients of edges
     return mag
 
-# Kernel operation using input operator of size 3*3
+# kernel operation using input operator of size 3*3
 def GetSobel(image, Sobel, width, height):
-    # Initialize the matrix
+    # initialize the matrix
     I_d = np.zeros((width, height), np.float32)
 
-    # For every pixel in the image
+    # for every pixel in the image
     for rows in range(width):
         for cols in range(height):
-            # Run the Sobel kernel for each pixel
+            # run the sobel kernel for each pixel
             if rows >= 1 or rows <= width-2 and cols >= 1 or cols <= height-2:
                 for ind in range(3):
                     for ite in range(3):
@@ -112,38 +105,25 @@ def GetSobel(image, Sobel, width, height):
     return I_d
 
 
-# Method implements the Harris Corner Detection algorithm
 def HarrisCornerDetection(image):
 
-    # The two Sobel operators - for x and y direction
+    # two sobel operators - for x and y direction
     SobelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     SobelY = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
     w, h = image.shape
 
-    # X and Y derivative of image using Sobel operator
+    # X and Y derivative of image using sobel operator
     ImgX = GetSobel(image, SobelX, w, h)
     ImgY = GetSobel(image, SobelY, w, h)
 
-    # # Eliminate the negative values
-    # There are multiple ways this can be done
-    # 1. Off setting with a positive value (commented out below)
-    # 2. Setting negative values to Zero (commented out)
-    # 3. Multiply by -1 (implemented below, found most reliable method)
-    # ImgX += 128.0
-    # ImgY += 128.0
+    # eliminating the negative values by multiplying with -1
     for ind1 in range(w):
         for ind2 in range(h):
             if ImgY[ind1][ind2] < 0:
                 ImgY[ind1][ind2] *= -1
-                # ImgY[ind1][ind2] = 0
             if ImgX[ind1][ind2] < 0:
                 ImgX[ind1][ind2] *= -1
-                # ImgX[ind1][ind2] = 0
-
-    # # Display the output results after Sobel operations
-    # cv2.imshow("SobelX", ImgX)
-    # cv2.imshow("SobelY", ImgY)
 
     ImgX_2 = np.square(ImgX)
     ImgY_2 = np.square(ImgY)
@@ -151,7 +131,7 @@ def HarrisCornerDetection(image):
     ImgXY = np.multiply(ImgX, ImgY)
     ImgYX = np.multiply(ImgY, ImgX)
 
-    #Use Gaussian Blur
+    #using gaussian blur for processing
     Sigma = 1.4
     kernelsize = (3, 3)
 
@@ -159,11 +139,10 @@ def HarrisCornerDetection(image):
     ImgY_2 = cv2.GaussianBlur(ImgY_2, kernelsize, Sigma)
     ImgXY = cv2.GaussianBlur(ImgXY, kernelsize, Sigma)
     ImgYX = cv2.GaussianBlur(ImgYX, kernelsize, Sigma)
-    # print(ImgXY.shape, ImgYX.shape)
 
     alpha = 0.06
     R = np.zeros((w, h), np.float32)
-    # For every pixel find the corner strength
+    # for every pixel find the corner strength
     for row in range(w):
         for col in range(h):
             M_bar = np.array([[ImgX_2[row][col], ImgXY[row][col]], [ImgYX[row][col], ImgY_2[row][col]]])
@@ -173,7 +152,7 @@ def HarrisCornerDetection(image):
 
 
 
-#### Main Program ####
+# main
 
 # opening file explorer
 file = easygui.fileopenbox()
@@ -188,39 +167,37 @@ frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 frame = cv2.bilateralFilter(frame,9,75,75)
 
 # calling canny edge detection function
-canny_img = Canny_detector(frame)
+canny_img = CannyDetector(frame)
 
 # saving canny edge detected image and converting image to float32 datatype
 cv2.imwrite('canny1.jpg',canny_img)
 
-# Get the first image
+# getting canny image
 firstimage = cv2.imread('canny1.jpg', cv2.IMREAD_GRAYSCALE)
 w, h = firstimage.shape
 
-# Covert image to color to draw colored circles on it
+# convert image to color to draw colored circles on it
 bgr = cv2.cvtColor(firstimage, cv2.COLOR_GRAY2RGB)
 
-# Corner detection
+# corner detection
 R = HarrisCornerDetection(firstimage)
 
-# Empirical Parameter
-# This parameter will need tuning based on the use-case
-CornerStrengthThreshold = 50000000
+# empirical parameter
+CornerStrengthThreshold = 200000000
 
-# Plot detected corners on image
+# plot detected corners on image
 radius = 1
 color = (0, 255, 0)  # Green
 thickness = 1
 
 PointList = []
-# Look for Corner strengths above the threshold
+# look for corner strengths above the threshold
 for row in range(w):
     for col in range(h):
         if R[row][col] > CornerStrengthThreshold:
-            # print(R[row][col])
             max = R[row][col]
 
-            # Local non-maxima suppression
+            # local non-maxima suppression
             skip = False
             for nrow in range(5):
                 for ncol in range(5):
@@ -230,12 +207,11 @@ for row in range(w):
                             break
 
             if not skip:
-                # Point is expressed in x, y which is col, row
+                # point is expressed in x, y which is col, row
                 cv2.circle(bgr, (col, row), radius, color, thickness)
                 PointList.append((row, col))
 
-# Display image indicating corners and save it
-#cv2.imshow("Corners", bgr)
+# displaying image indicating corners and save it
 outname = "harrisimg" + ".png"
 cv2.imwrite(outname, bgr)
 
